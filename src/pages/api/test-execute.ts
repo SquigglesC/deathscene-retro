909e5ac8b0cdaeb83714c7e855a45c1e11ab21ab
SquigglesC <erraserinc@gmail.com>
2025-05-21 15:02:49 -0700

    changed test-execute query

--- src/pages/api/test-execute.ts
+++ src/pages/api/test-execute.ts
@@ -1,9 +1,52 @@
 import { execute } from "../../graphql/execute";
-import { AllProductsQuery } from "../../features/shop/storefront/getAllProducts";
+//import { AllProductsQuery } from "../../features/shop/storefront/getAllProducts";
+const query = `
+  query AllProducts {
+    products(first: 100) {
+      nodes {
+        id
+        title
+        handle
+        updatedAt
+        tags
+        totalInventory
+        featuredImage {
+          url
+          altText
+          width
+          height
+        }
+        images(first: 2) {
+          edges {
+            node {
+              url
+              altText
+              width
+              height
+            }
+          }
+        }
+        priceRange {
+          minVariantPrice {
+            amount
+          }
+        }
+        variants(first: 100) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 
 export async function GET() {
   try {
-    const data = await execute(AllProductsQuery);
+    const data = await execute(query);
     return new Response(JSON.stringify(data, null, 2), {
       status: 200,
       headers: {
